29f8afa539ec4dea0caa5085fab91880461901b6
teo-merge <teodoro.calle@mergedev.co>
2024-02-12 17:44:08 -0500

    first commit

---
+++ Text-SwiftUI/Text_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_SwiftUIApp.swift
+//  Text-SwiftUI
+//
+//  Created by Teodoro Calle Lara on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Text_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
