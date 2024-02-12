aabfd0463a2e571358cadb93729f7dedd62025da
teo-merge <teodoro.calle@mergedev.co>
2024-02-12 18:13:43 -0500

    se agregan varios modificadores

--- Text-SwiftUI/ContentView.swift
+++ Text-SwiftUI/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. ")
+                .font(.largeTitle)
+                .underline(color: Color.red)
+                .rotation3DEffect(
+                    .degrees(25),
+                    axis: (x: 1.0, y: 0.0, z: 0.0))
+                .shadow(color: .gray, radius: 2, x: 0, y: 10)
+                .lineLimit(10)
+                .lineSpacing(20)
         }
         .padding()
     }
