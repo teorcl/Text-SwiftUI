7f86c1b248bfe5df9a918b34a5d72f10948b0e20
teo-merge <teodoro.calle@mergedev.co>
2024-02-13 10:18:43 -0500

    Se agregan fechas en varios formatos, tambien la hora en Text

--- Text-SwiftUI/ContentView.swift
+++ Text-SwiftUI/ContentView.swift
@@ -10,15 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. ")
-                .font(.largeTitle)
-                .underline(color: Color.red)
-                .rotation3DEffect(
-                    .degrees(25),
-                    axis: (x: 1.0, y: 0.0, z: 0.0))
-                .shadow(color: .gray, radius: 2, x: 0, y: 10)
-                .lineLimit(10)
-                .lineSpacing(20)
+            Text(Date(), style: .date)
+            Text(Date(), style: .timer)
+            Text(Date().addingTimeInterval(3600), style: .timer)
+            Text(Date(), style: .time)
         }
         .padding()
     }
